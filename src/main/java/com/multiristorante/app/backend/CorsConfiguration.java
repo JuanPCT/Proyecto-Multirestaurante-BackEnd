08cfcf6ee94f1bbba2d4ea3d3b53299daae1c7e2
Juan Pablo Correa Tarazona <89850618+JuanPCT@users.noreply.github.com>
2022-11-13 11:24:46 -0500

    Restaurante Controller y CORS

---
+++ src/main/java/com/multiristorante/app/backend/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.multiristorante.app.backend;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ 
+//@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowCredentials(true);
+    }
+
+}
--- src/main/java/com/multiristorante/app/backend/repository/RestauranteRepository.java
+++ src/main/java/com/multiristorante/app/backend/repository/RestauranteRepository.java
@@ -1,7 +1,6 @@
 package com.multiristorante.app.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.multiristorante.app.backend.Entities.Restaurante;
 
 public interface RestauranteRepository extends JpaRepository<Restaurante, Integer>{
