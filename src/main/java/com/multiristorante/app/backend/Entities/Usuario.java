01595fc7d89749d01cc8f26367cefd2ae9f2f4ac
Juan Pablo Correa Tarazona <89850618+JuanPCT@users.noreply.github.com>
2022-12-02 15:28:50 -0500

    changes

--- src/main/java/com/multiristorante/app/backend/Entities/Usuario.java
+++ src/main/java/com/multiristorante/app/backend/Entities/Usuario.java
@@ -3,22 +3,34 @@ package com.multiristorante.app.backend.Entities;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
 import java.util.Date;
 
 @Entity
 @Data
 @Table(name = "usuario")
 public class Usuario {
+
     @Id
     private int documento;
+    @NotEmpty
     private String nombre;
+
     private String apellido;
+
     private String telefono;
+
     private String email;
+
     private String password;
+
     private boolean estado;
+
     private Date fecha_nacimiento;
+
     private Date fecha_creacion;
+    
     @ManyToOne
     @JoinColumn(name="Rol_id")
     private Rol id;
